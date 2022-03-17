93b232d02974877c77b8ebc343eae6ee45c415ac
avargasm1 <avargasm@unsa.edu.pe>
2022-03-17 16:30:20 -0500

    Se añadió la subida de fotos, previsualización de fotos, fecha y hora picker

--- src/modules/modulo 1/AddReporte.jsx
+++ src/modules/modulo 1/AddReporte.jsx
@@ -1,13 +1,16 @@
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
+import { StyleSheet, Text, View, useWindowDimensions, Image } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import TemplateScreen from '../../Template/TemplateScreen'
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
-import { FormControl, Input, VStack, Pressable, Icon, Image, TextArea, FlatList, HStack, ScrollView } from 'native-base';
+import { FormControl, Input, VStack, Pressable, Icon, TextArea, FlatList, HStack, ScrollView } from 'native-base';
 import TemplateScreenNoHeader from '../../Template/TemplateScreenNoHeader';
 import { Ionicons } from '@expo/vector-icons';
 import { getEquiment, getSuperIntendent } from '../services/misServicios';
 import SearchableDropDown from 'react-native-searchable-dropdown';
 import { Card, Modal, Button } from '@ui-kitten/components';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import * as ImagePicker from 'expo-image-picker';
+import ImageView from 'react-native-image-view';
 
 const AddReporte = () => {
 
@@ -26,7 +29,8 @@ const AddReporte = () => {
         take_actions: '',
         results: '',
         equipment_id: '',
-        attachments: ''
+        attachments: '',
+        foto1: []
     })
     const [botonH, setBotonH] = useState(false);
     const [superIntendente, setSuperIntendente] = useState([])
@@ -61,6 +65,76 @@ const AddReporte = () => {
     const [modalBuscarEquipos, setModalBuscarEquipos] = useState(false)
     const [inputEquipos, setInputEquipos] = useState('')
     const [miValorModalEquipos, setMiValorModalEquipos] = useState('')
+
+    // DATEPICKER CONSTANTES
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+    //////////////////////////////////
+
+    const [pickedImagePath, setPickedImagePath] = useState(miObjeto.foto1[0]?.base64);
+    const [dataFoto, setDataFoto] = useState(
+
+        {
+            model_type: 2,
+            attachmentable_type: "App\\Models\\equipos\\Troubleshooting",
+            attachmentable_id: 1,
+            base64: ""
+        }
+
+    )
+
+    const showImagePicker = async () => {
+        // Apreguntar por los permisos
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Te has negado a permitir que esta aplicación acceda a tus fotos!");
+            return;
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: false,
+            base64: true,
+        });
+        if (!result.cancelled) {
+            setPickedImagePath(result.uri);
+
+            const source = { uri: 'data:image/jpeg;base64,' + result.base64 };
+            // console.warn(source.uri);
+            dataFoto.base64 = source.uri
+            miObjeto.foto1[0] = dataFoto
+        }
+    }
+    const [isImageViewVisible, setisImageViewVisible] = useState(false);
+    const images = [
+        {
+          source: {
+            uri: pickedImagePath,
+          },
+          title: 'Paris',
+          width: 806,
+          height: 720,
+        },
+      ];
     return (
         <>
 
@@ -78,13 +152,30 @@ const AddReporte = () => {
                                 </View>
                                 <VStack width="100%" mx="3" maxW="300px" my="4">
                                     <FormControl>
-                                        <FormControl.Label _text={{
-                                            bold: true
-                                        }}>Fecha y Hora</FormControl.Label>
-                                        <Input placeholder="John" onChangeText={value => setData({
-                                            ...formData,
-                                            name: value
-                                        })} />
+                                        <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+                                            <View style={{ width: '55%', marginRight: 5 }}><FormControl.Label _text={{
+                                                bold: true
+                                            }}>Fecha <Pressable onPress={showDatepicker}><Icon as={Ionicons} size={6} name='calendar-outline' /></Pressable></FormControl.Label>
+                                                {show && (
+                                                    <DateTimePicker
+                                                        testID="dateTimePicker"
+                                                        value={date}
+                                                        mode={mode}
+                                                        is24Hour={true}
+                                                        onChange={onChange}
+                                                    />
+                                                )}
+                                                <Text style={{ backgroundColor: 'rgba(229, 227, 227, 0.9)', textAlign: 'center', borderRadius: 5, padding: 10 }}>{date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()}</Text>
+                                            </View>
+                                            <View style={{ width: '40%', marginLeft: 5 }}><FormControl.Label _text={{
+                                                bold: true
+                                            }}>Hora <Pressable onPress={showTimepicker}><Icon as={Ionicons} size={6} name='time-outline' /></Pressable></FormControl.Label>
+                                                <Text style={{ backgroundColor: 'rgba(229, 227, 227, 0.9)', textAlign: 'center', borderRadius: 5, padding: 10 }}>{date.getHours() + ':' + date.getMinutes()}</Text>
+                                            </View>
+
+
+                                        </View>
+
 
                                         <FormControl.ErrorMessage _text={{
                                             fontSize: 'xs'
@@ -233,7 +324,9 @@ const AddReporte = () => {
                                 </View>
                                 <ScrollView horizontal>
                                     <FormControl style={{ flexDirection: 'row', margin: 10 }}>
+
                                         <View style={{ marginRight: 20 }}>
+
                                             <FormControl.Label _text={{
                                                 bold: true
                                             }}>Evidencia N° 1</FormControl.Label>
@@ -255,12 +348,21 @@ const AddReporte = () => {
                                                 justifyContent: 'center',
                                                 alignItems: 'center',
                                             }}>
-                                                <View style={{ flexDirection: 'row' }}>
-                                                    <Pressable style={{ marginRight: 10 }}>
-                                                        <Icon as={Ionicons} size={45} name="image-outline" />
+                                                {
+                                                    pickedImagePath !== '' && <Image
+                                                        source={{ uri: pickedImagePath }}
+                                                        style={styles.image}
+                                                    />
+                                                }
+                                                <View style={{ flexDirection: 'row',backgroundColor:'rgba(255,255,255,0.5)'}}>
+                                                    <Pressable onPress={showImagePicker} style={{ marginRight: 10 }}>
+                                                        <Icon as={Ionicons} size={35} name="image-outline" />
                                                     </Pressable>
                                                     <Pressable style={{ marginLeft: 10 }}>
-                                                        <Icon as={Ionicons} size={45} name="camera-outline" />
+                                                        <Icon as={Ionicons} size={35} name="camera-outline" />
+                                                    </Pressable>
+                                                    <Pressable onPress={()=>setisImageViewVisible(true)} style={{ marginLeft: 10 }}>
+                                                        <Icon as={Ionicons} size={35} name="scan" />
                                                     </Pressable>
                                                 </View>
                                             </View>
@@ -293,7 +395,17 @@ const AddReporte = () => {
                                                     <Pressable style={{ marginLeft: 10 }}>
                                                         <Icon as={Ionicons} size={45} name="camera-outline" />
                                                     </Pressable>
+                                                   
                                                 </View>
+                                                <ImageView
+                                                    isSwipeCloseEnabled={true}
+                                                    onClose={() => setisImageViewVisible(false)}
+                                                    images={images}
+                                                    imageIndex={0}
+                                                    isPinchZoomEnabled={true}
+                                                    isVisible={isImageViewVisible}
+                                                    
+                                                />
                                             </View>
                                         </View>
 
@@ -442,7 +554,7 @@ const AddReporte = () => {
                                 //items container style you can pass maxHeight
                                 //to restrict the items dropdown hieght
                                 maxHeight: '50%',
-                                
+
                             }}
                             items={equipos}
                             //mapping of item array
@@ -495,6 +607,11 @@ const styles = StyleSheet.create({
     backdrop: {
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
-
+    image: {
+        width: '95%',
+        height: '97%',
+        resizeMode: 'cover',
+        position: 'absolute'
+    }
 
 })
