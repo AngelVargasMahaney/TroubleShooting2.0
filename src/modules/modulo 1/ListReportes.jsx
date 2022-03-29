b317c57609dd849985470d012f1fa5cdc7ca987d
avargasm1 <avargasm@unsa.edu.pe>
2022-03-29 12:16:35 -0500

    Se añadió la vista de lista de reportes completamente dinámica con SwipeList

--- src/modules/modulo 1/ListReportes.jsx
+++ src/modules/modulo 1/ListReportes.jsx
@@ -1,27 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Dimensions, TouchableOpacity, ScrollView } from "react-native";
+import { NativeBaseProvider, Box, Text, Pressable, Heading, IconButton, Icon, HStack, Avatar, VStack, Spacer, Center, useToast } from "native-base";
+import { SwipeListView } from "react-native-swipe-list-view";
+import { MaterialIcons, Ionicons, Entypo } from "@expo/vector-icons";
+
 import TemplateScreen from '../../Template/TemplateScreen'
 import { getSearch, getTroubleShooting } from '../services/misServicios'
 import { useNavigation } from '@react-navigation/native'
+import { SCLAlert, SCLAlertButton } from 'react-native-scl-alert';
 
 
 const ListReportes = () => {
 
-    const [Reportes, setReportes] = useState([])
-    const navigation = useNavigation();
 
+    const [listData, setListData] = useState([]);
     const traerTroubles = () => {
-       
+
         getTroubleShooting().then(rpta => {
-            setReportes(rpta.data.data)
-            console.log("TROUBLES"+rpta.data.data)
+            setListData(rpta.data.data)
+            console.log(rpta.data.data)
         })
     }
 
 
     useEffect(() => {
         traerTroubles()
-        traerBusqueda ()
+        traerBusqueda()
     }, [])
 
     const [filteredDataSource, setFilteredDataSource] = useState([]);
@@ -29,27 +33,141 @@ const ListReportes = () => {
     let [service, setService] = React.useState("");
 
     const traerBusqueda = () => {
-      
-        getSearch( 1, "LINER").then(rpta => {
-        //getSearch( service, search).then(rpta => {
+
+        getSearch(1, "LINER").then(rpta => {
+            //getSearch( service, search).then(rpta => {
             setFilteredDataSource(rpta.data.data)
-           
-            console.log("searrch"+rpta.data.data)
+
+            // console.log("searrch" + rpta.data.data)
         })
     }
+    const navigation = useNavigation();
+    const toast = useToast();
 
+
+    //SWIPELIST
+
+    const closeRow = (rowMap, rowKey) => {
+        if (rowMap[rowKey]) {
+            rowMap[rowKey].closeRow();
+        }
+    };
+
+    const deleteRow = (rowMap, rowKey) => {
+        closeRow(rowMap, rowKey);
+        const newData = [...listData];
+        const prevIndex = listData.findIndex(item => item.key === rowKey);
+        newData.splice(prevIndex, 1);
+        setListData(newData);
+    };
+
+    const onRowDidOpen = rowKey => {
+        // console.log("This row opened", rowKey);
+    };
+
+    const [show, setShow] = useState(false)
+    const renderItem = ({
+        item,
+        index
+    }) =>
+        <Box>
+            <Pressable onPress={() => toast.show({
+                description: "Deslice para más acciones"
+            })} _dark={{
+                bg: "coolGray.800"
+            }} _light={{
+                bg: "white"
+            }}>
+                <Box pl="4" pr="5" py="2" >
+                    <HStack alignItems="center" space={3}>
+                        <Avatar size="48px" source={{
+                            uri: item.equipment?.cover
+                        }} />
+                        <VStack width="85%">
+                            <Text color="coolGray.800" _dark={{
+                                color: "warmGray.50"
+                            }} bold>
+                                {(item.event).slice(0, 25) + " ..."}
+                            </Text>
+                            <Text color="coolGray.600" _dark={{
+                                color: "warmGray.200"
+                            }}>
+                                {item.operators}
+                            </Text>
+                        </VStack>
+                        <Spacer />
+                        <Text fontSize="xs" color="coolGray.800" _dark={{
+                            color: "warmGray.50"
+                        }} alignSelf="flex-start">
+                            {item.timeStamp}
+                        </Text>
+                    </HStack>
+                </Box>
+            </Pressable>
+        </Box>
+
+    const renderHiddenItem = (data, rowMap) => <HStack flex="1" pl="2">
+        <Pressable w="70" ml="auto" bg="coolGray.200" justifyContent="center" onPress={() => closeRow(rowMap, data.item.key)} _pressed={{
+            opacity: 0.5
+        }}>
+            <VStack alignItems="center" space={2}>
+                <Icon as={<Entypo name="eye" />} size="xs" color="coolGray.800" />
+                <Text fontSize="xs" fontWeight="medium" color="coolGray.800">
+                    More
+                </Text>
+            </VStack>
+        </Pressable>
+        <Pressable w="70" bg="red.500" justifyContent="center" onPress={() => setShow(true)} _pressed={{
+            opacity: 0.5
+        }}>
+            <VStack alignItems="center" space={2}>
+                <Icon as={<MaterialIcons name="delete" />} color="white" size="xs" />
+                <Text color="white" fontSize="xs" fontWeight="medium">
+                    Delete
+                </Text>
+            </VStack>
+        </Pressable>
+    </HStack>;
 
 
     return (
-        
+
         <>
             <TemplateScreen />
             <View style={styles.container}>
-                <Text>{filteredDataSource[0].event}</Text>
+                {/* <Text>{filteredDataSource[0]?.event}</Text> */}
+
+
+                <SwipeListView
+                    data={listData}
+                    renderItem={renderItem}
+                    renderHiddenItem={renderHiddenItem}
+                    rightOpenValue={-130} previewRowKey={"0"}
+                    previewOpenValue={-40}
+                    previewOpenDelay={3000}
+                    onRowDidOpen={onRowDidOpen} />
+
+
             </View>
+            <SCLAlert
+                show={show}
+                onRequestClose={() => setShow(false)}
+                theme="danger"
+                title="Aviso"
+                subtitle="Está seguro que desea eliminar este reporte?"
+                headerIconComponent={<Ionicons name="trash-outline" size={32} color="white" />}
+            >
+                <SCLAlertButton theme="info" onPress={() => {
+                    console.log('Ingresé')
+                    setShow(false);
+                   
+
+                }}>Continuar</SCLAlertButton>
+            </SCLAlert>
         </>
     )
 }
+
 
 export default ListReportes
 
@@ -58,6 +176,16 @@ const styles = StyleSheet.create({
         position: 'absolute',
         marginTop: 130,
         marginHorizontal: 20,
+    },
+    shadows: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
 
+        elevation: 5,
     },
 })
